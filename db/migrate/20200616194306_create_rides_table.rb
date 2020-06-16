4335663b03e6ed535d08942c0cb8c4f5eae5f062
Megan Comeau <mlcomeau99@gmail.com>
2020-06-16 19:43:39 +0000

    Automatically backed up by Learn

--- db/migrate/20200616194306_create_rides_table.rb
+++ db/migrate/20200616194306_create_rides_table.rb
@@ -1,6 +1,6 @@
 class CreateRidesTable < ActiveRecord::Migration[5.0]
   def change
-    create_table :rides_tables do |t|
+    create_table :rides do |t|
       t.integer :passenger_id
       t.integer :taxi_id
     end
